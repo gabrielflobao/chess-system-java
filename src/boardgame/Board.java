ce9407fe64343488742d5960d804a8923c6403b8
lobaoff <lobaogabriel100@gmail.com>
2021-01-10 18:24:41 -0300

    implementado borda e peça

---
+++ src/boardgame/Board.java
@@ -0,0 +1,28 @@
+package boardgame;
+
+public class Board {
+	private int linhas;
+	private int colunas;
+	private Piece[][] peças;
+	public Board(int linhas, int colunas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		peças = new Piece [linhas][colunas];
+		
+		
+	}
+	public int getLinhas() {
+		return linhas;
+	}
+	public void setLinhas(int linhas) {
+		this.linhas = linhas;
+	}
+	public int getColunas() {
+		return colunas;
+	}
+	public void setColunas(int colunas) {
+		this.colunas = colunas;
+	}
+	
+	
+}
